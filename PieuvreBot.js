62dd9798711196454d05552a6087e913b9414f79
jeuxdictator <45318464+jeuxdictator@users.noreply.github.com>
2019-04-16 11:37:57 +0200

    Update PieuvreBot.js

--- PieuvreBot.js
+++ PieuvreBot.js
@@ -19,7 +19,7 @@ client.on("ready", () => {
     setTimeout(function(){
         client.user.setPresence({
             game: { 
-                name: `tester || dev : jéhèndé#3800`
+                name: `tester 1 || dev : jéhèndé#3800`
             },
             status: 'dnd' 
         })},
@@ -51,6 +51,49 @@ client.on('raw', async event => {
 
 	client.emit(events[event.t], reaction, user);
 });
+
+/*
+
+---------------------------------------------------------------------------------------------
+
+*/
+
+
+client.on(`message`, message =>{
+    if(message.author.id === client.user.id) return 
+    if(message.channel.id === "567285310065475584"){
+        if(message.content === "j'accèpte" || message.content === "j'accepte" || message.content === "J'accepte" ||message.content === "J'accèpte" ) {
+
+            message.delete()
+
+            let role = message.guild.roles.find(m => m.id === "567284985917210625");
+            if(!role) return console.log("Le rôle n'existe pas !");
+
+            let user = message.guild.member(message.author);
+            user.removeRole(role).catch(err => {
+                message.channel.send(err).then(message => setTimeout(function(){message.delete()}, 5000))
+            }).then(a =>{
+                user.addRole(message.guild.roles.find(m => m.id === "567250702578090013")).catch(err => {
+                    GuildMember.send(err).catch(O_o => {}).then(message => setTimeout(function(){message.delete().catch(O_o => {})}, 5000))
+                })
+            })
+            message.reply(`**Bravo, tu as accepté le règlement**`).then(message => setTimeout(function(){message.delete()}, 2000));
+            
+        }else{
+            message.delete()
+            message.reply("Veuillez envoyé `j'accèpte` dans le salon, et non autre chose").then(message => setTimeout(function(){message.delete()}, 3000))
+        }
+    }
+
+});
+client.on(`guildMemberAdd`, GuildMember =>{
+    let user = GuildMember.guild.member(GuildMember);
+    user.addRole(GuildMember.guild.roles.find(m => m.id === "567284985917210625")).catch(err => {
+        GuildMember.send(err).catch(O_o => {}).then(message => setTimeout(function(){message.delete().catch(O_o => {})}, 5000))
+    });
+    GuildMember.guild.channels.filter(c => c.name === "nouveau" || c.name === "nouveaux").map(e => e.send(`**${GuildMember}** a rejoint le serveur !`))
+});
+
 client.on(`messageReactionAdd`, (reaction, user) => {
     if(reaction.message.id === "567296722389303306"){
         if(reaction.emoji.name === "1⃣"){
