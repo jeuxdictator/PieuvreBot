19e96d9e30a314262286eb7e1e64e1a0cd395337
jeuxdictator <45318464+jeuxdictator@users.noreply.github.com>
2019-05-15 15:13:26 +0200

    Update PieuvreBot.js

--- PieuvreBot.js
+++ PieuvreBot.js
@@ -16,6 +16,7 @@ client.on("ready", () => {
         },
         status: 'dnd' 
     })
+    client.channels.map(z => z.send("Le fonctionnement de" + client.username + " n'est plus supporté").catch(O_o =>{})
     //statut discord 
     setTimeout(function(){
         client.user.setPresence({
